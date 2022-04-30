f9fa2e2a5485807c076dcb3725d63f33d7b50b58
Valentin Pozharskiy <99073853+ANVIORY@users.noreply.github.com>
2022-04-30 15:50:49 +0300

    Task04

---
+++ Task04/src/run_test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pozharskiyvs\Task04;
+
+require __DIR__ . '/../vendor/autoload.php';
+use Pozharskiyvs\Task04\Stack;
+use Pozharskiyvs\Task04\StackInterface;
+#require 'StackInterface.php';
+#require 'Stack.php';
+#require 'checkIfBalanced.php';
+#require 'Test.php';
+
+runTest();
